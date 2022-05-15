7767def7f325e87004656fa72fa9b860a6f34895
VisualDad <thomas-blanck@gmx.de>
2022-05-15 20:49:09 +0200

    implement multistep webform with SessionWizardView from formtools

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-
+from homepage.forms import ContactForm1, ContactForm2
+from homepage.views import ContactWizard
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='index'),
+    path('register/', ContactWizard.as_view([ContactForm1, ContactForm2])),
+
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from formtools.wizard.views import SessionWizardView
 
-# Create your views here.
-def index(request):
-    return HttpResponse("Hello, world. You're at the  index.")
+def home(request):
+    context = {'latest_question_list': 'test'}
+    return render(request, 'home.html', context)
 
+
+# multistep form
+class ContactWizard(SessionWizardView):
+    template_name = "register.html"
+    def done(self, form_list, **kwargs):
+        form_data = process_form_data(form_list)
+        return render(self.request, 'done.html', {'form_data': form_data})
+
+def process_form_data(form_list):
+    form_data = [form.cleaned_data for form in form_list]
+    print(form_data)
+    return form_data
